d7281eaefc79060846e511b2d046c21ea8c92711
SrinivasMuchu <107020456+SrinivasMuchu@users.noreply.github.com>
2022-12-02 19:02:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import {Router, Route, Routes} from 'react-router-dom'
+import Home from './components/Home';
+import Profile from './components/Profile';
+function App() {
+  return (
+    <div className="bg-black h-screen overflow-hidden w-screen  text-slate-200">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
